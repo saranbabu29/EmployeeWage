d61af8b68ef3d443a934ddb3dc8b8561a1c350ad
saranbabu29 <sharanraj982@gmail.com>
2022-07-31 09:29:51 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int emp_present = 0;
+            Random random = new Random();
+            int value = random.Next(0, 1);
+            if (value == emp_present)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
