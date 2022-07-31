9967bbdd110f084beef3d1bd57d4d1e4b9b48771
saranbabu29 <sharanraj982@gmail.com>
2022-07-31 11:45:17 +0530

    forth commit

--- Program.cs
+++ Program.cs
@@ -1,22 +1,36 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace uc3
 {
     class Program
     {
+        public const int is_Full_Time = 0;
+        public const int is_Part_Time = 1;
+        public const int per_hour_Wage = 20;
+
         static void Main(string[] args)
         {
-            int emp_present = 0;
+            int emphr;
             Random random = new Random();
-            int value = random.Next(0, 1);
-            if (value == emp_present)
+            int value = random.Next(0, 2);
+
+            switch (value)
             {
-                Console.WriteLine("Employee is present");
+                case is_Full_Time:
+                    emphr = 8;
+                    Console.WriteLine("The FullTime Emp Wage is =" + (per_hour_Wage * emphr));
+                    break;
+                case is_Part_Time:
+                    emphr = 4;
+                    Console.WriteLine("The PartTime Emp Wage is =" + (per_hour_Wage * emphr));
+                    break;
+                default:
+                    emphr = 0;
+                    break;
+
             }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-            }
+
         }
+
     }
 }
